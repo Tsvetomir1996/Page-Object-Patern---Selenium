1f96c88043af832403cd5b947c6adcc98a5e4ba5
Tsvetomir1996 <158475119+Tsvetomir1996@users.noreply.github.com>
2024-02-01 20:33:33 +0200

    Create Browser.java

---
+++ Selenium/src/main/java/Selenium/utils/Browser.java
@@ -0,0 +1,26 @@
+package selenium.utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Browser {
+
+    public static WebDriver driver;
+    public static void openBrowser() throws MalformedURLException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        driver = new RemoteWebDriver(new URL("http://localhost:4545"), chromeOptions);
+        driver.get("http://www.google.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    public static void closeBrowser() {
+        driver.quit();
+    }
+
+}
